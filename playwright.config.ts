a27742ed45af821b76fe8ab05dcf11f2f6f5f872
Charl Ritter <charlritter@hotmail.com>
2024-05-30 07:36:58 +0000

    Use next start instead of dev for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'yarn dev',
+    command: 'yarn start',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI
   }
